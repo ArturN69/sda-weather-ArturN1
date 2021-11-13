7e0a883a942286efd44d60f9f238f48296c393fd
Artur Noworyta <arturno69@gmail.com>
2021-11-13 11:29:21 +0100

    UserInterface class create

---
+++ src/main/java/com/sda/weather/UserInterface.java
@@ -0,0 +1,15 @@
+package com.sda.weather;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    public void run() {
+
+        System.out.println("Aplikacja pogodowa jest uruchomiona\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
