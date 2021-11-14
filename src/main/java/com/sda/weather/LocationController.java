19f1d0219393d5e04586262195b4bc58d2043598
Artur Noworyta <arturno69@gmail.com>
2021-11-14 09:16:34 +0100

    first on sunday

--- src/main/java/com/sda/weather/LocationController.java
+++ src/main/java/com/sda/weather/LocationController.java
@@ -5,6 +5,7 @@ public class LocationController {
     public String addNewLocation(String data) { // a data is a JSON format
         // todo deserialize data (as a JSON) to a LocationDTO -> use ObjectMapper
         // todo pass those data to eg. LocationService as single values (avoid passing DTOs to a service layer)
+        // ne wObjectmapper alt-enter i z mavena zaciągnąć biblioteki, reszta z Michała daily
 
         return data;
     }
