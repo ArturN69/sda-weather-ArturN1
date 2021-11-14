09a340e72954a0919fef8fa4ad7f370d11a0f5c2
Artur Noworyta <arturno69@gmail.com>
2021-11-14 11:17:40 +0100

    next one try

--- src/main/java/com/sda/weather/LocationController.java
+++ src/main/java/com/sda/weather/LocationController.java
@@ -1,14 +1,35 @@
 package com.sda.weather;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class LocationController {
 
     private final ObjectMapper objectMapper;
 
+    public LocationController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     public String addNewLocation(String data) { // a data is a JSON format
-        // todo deserialize data (as a JSON) to a LocationDTO -> use ObjectMapper
+        // deserialize data (as a JSON) to a LocationDTO -> use ObjectMapper
         // todo pass those data to eg. LocationService as single values (avoid passing DTOs to a service layer)
+        try {
+            LocationDTO locationDTO = objectMapper.readValue(data, LocationDTO.class);
+            String city = locationDTO.getCity();
+            String region = locationDTO.getRegion();
+            String country = locationDTO.getCountry();
+            String latitude = locationDTO.getLatitude();
+            String longitude = locationDTO.getLongitude();
 
+            locationDTO entry
+            LocationDTO locationDTO1 = new LocationDTO();
+
+
+        } catch (JsonProcessingException e) {
+            String errorMessage = e.getMessage();
+            return "{\"message\": \"" + errorMessage + "\"}";
+        }
 
         return data;
     }
--- src/main/java/com/sda/weather/WeatherApplication.java
+++ src/main/java/com/sda/weather/WeatherApplication.java
@@ -1,9 +1,12 @@
 package com.sda.weather;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class WeatherApplication {
     public static void main(String[] args) {
         // create a new LocationController and pass it to the UserInterface by a constructor
-        LocationController locationController = new LocationController();
+        ObjectMapper objectMapper = new ObjectMapper();
+        LocationController locationController = new LocationController(objectMapper);
         UserInterface userInterface = new UserInterface(locationController);
         userInterface.run();
     }
